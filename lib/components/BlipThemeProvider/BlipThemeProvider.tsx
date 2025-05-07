f3ccbe95489fc50887c81a9f8f2093d2eaff65b9
Chris Engelsma <chris.engelsma@gmail.com>
2025-05-07 12:04:19 -0500

    Test

--- lib/components/BlipThemeProvider/BlipThemeProvider.tsx
+++ lib/components/BlipThemeProvider/BlipThemeProvider.tsx
@@ -4,10 +4,6 @@ import clsx from 'clsx';
 
 type Theme = 'basic' | 'inverted';
 
-const getBodyTheme = (): Theme => {
-  return ( document.body.getAttribute('data-blip-theme') as Theme ) || 'basic';
-};
-
 export const withBlipTheme = (Component: React.ComponentType<any>) => {
   return (props: any) => (
     <BlipThemeProvider>
@@ -17,35 +13,12 @@ export const withBlipTheme = (Component: React.ComponentType<any>) => {
 };
 
 export const BlipThemeProvider = (props: any) => {
-  const [ theme, setTheme ] = React.useState<Theme>(getBodyTheme());
-
-  React.useEffect(() => {
-    const updateTheme = () => {
-      setTheme(getBodyTheme());
-    };
-
-    // Initial theme set
-    updateTheme();
-
-    // Set up a MutationObserver to watch for changes to the data-blip-theme attribute
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'data-blip-theme') {
-          updateTheme();
-        }
-      });
-    });
-
-    observer.observe(document.body, { attributes: true, attributeFilter: [ 'data-blip-theme' ] });
-
-    // Clean up the observer when the component unmounts
-    return () => observer.disconnect();
-  }, []);
+  const [ theme, setTheme ] = React.useState<Theme>(props?.theme ?? 'basic');
 
   return <div
     className={ clsx(
-      'BlipThemeWrapper',
-      `BlipThemeWrapper--${ theme }`
+      'BlipThemeProvider',
+      `BlipThemeProvider--${ theme }`
     ) }>
     { props?.children }
   </div>;
